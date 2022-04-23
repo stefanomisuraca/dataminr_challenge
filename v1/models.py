9ce058a631564a4881e294c40b51d9f30c7a0235
Stefano Misuraca <stefano.misuraca92@gmail.com>
2022-04-23 11:19:54 +0200

    subscriptions and alerts models

--- v1/models.py
+++ v1/models.py
@@ -1,2 +1,40 @@
+from ast import Call
+from datetime import datetime
 from django.db import models
-from django.contrib.postgres import fields as pg_fields
+from typing import Callable
+
+import logging
+logger = logging.getLogger("dataminr")
+
+class TimeStampMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Subscriptions(TimeStampMixin):
+    """Subscription model"""
+   
+    address = models.EmailField(max_length=254)
+    location = models.CharField(max_length=254)
+    observer = models.JSONField(default=list)
+
+
+class Alerts(TimeStampMixin):
+    """Alerts model"""
+
+    subscription = models.ForeignKey(Subscriptions, on_delete=models.CASCADE)
+    sent_date = models.DateTimeField(null=True)
+
+    def send_alert(self) -> None:
+        logger.info("Alert sent!")
+        self.sent_date = datetime.now()
+        self.save()
+
+    def check_conditions(self, callback: Callable) -> None:
+        return self.send_alert() if callback else logger.info("Condition not met yet")
+
+
+
+
