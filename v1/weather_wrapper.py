ea58e858ff8ab24e9ff35c0d3e3e028b4a06eb17
Stefano Misuraca <stefano.misuraca92@gmail.com>
2022-04-24 13:50:22 +0200

    various fixes

--- v1/weather_wrapper.py
+++ v1/weather_wrapper.py
@@ -2,7 +2,6 @@ import os
 from typing import Dict
 from django.conf import settings
 import requests
-
 import logging
 logger = logging.getLogger("dataminr")
 
@@ -10,9 +9,9 @@ class WeatherWrapper:
     def __init__(self) -> None:
         self.api_key = os.getenv("OPEN_WEATHER_KEY")
         self.api_url = settings.OPEN_WEATHER_API.get("url")
-        print(self.api_url)
         
     def get_weather(self, *, location:str) -> Dict[str, int]:
+
         api_endpoint = f"{self.api_url}?q={location}&APPID={self.api_key}&units=metric"
         try:
             result = requests.get(api_endpoint, timeout=5)
